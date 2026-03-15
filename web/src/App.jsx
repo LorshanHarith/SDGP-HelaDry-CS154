39f425a894dd5411033bdbf8533569661fe7e554
Drian704 <drianlogaeswaran@gmail.com>
2026-03-15 15:23:30 +0530

    app.jsx

---
+++ web/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Features from "./components/Features"
+import Team from "./components/Team"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Team />
+    </>
+  )
+}
+
+export default App
