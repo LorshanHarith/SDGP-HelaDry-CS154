76b620637180fa72f8104faa064df3b0e2b18cc9
Drian704 <drianlogaeswaran@gmail.com>
2026-03-16 07:16:18 +0530

    updated app.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,15 +1,20 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Features from "./components/Features"
+import SensorChart from "./components/Sensorchart"
+import FarmerDashboard from "./components/FarmerDashboard"
 import Team from "./components/Team"
 
-function App() {
-  return (
+function App(){
+
+  return(
     <>
-      <Navbar />
-      <Hero />
-      <Features />
-      <Team />
+      <Navbar/>
+      <Hero/>
+      <Features/>
+      <SensorChart/>
+      <FarmerDashboard/>
+      <Team/>
     </>
   )
 }
