ac30dd7f19a25fefa32dd3854448c660ad9caf4a
Drian704 <drianlogaeswaran@gmail.com>
2026-03-17 11:21:42 +0530

    footer

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -1,20 +1,4 @@
-function Footer(){
-
-  return(
-
-    <footer className="bg-gray-900 text-white text-center py-6">
-
-      <p>
-        © 2026 HelaDry Project
-      </p>
-
-      <p className="text-gray-400 text-sm mt-1">
-        Smart Solar Crop Dehydration System
-      </p>
-
-    </footer>
-
-  )
-}
-
-export default Footer
+<footer>
+<div class="foot-brand">🌿 Hela<span>Dry</span></div>
+<p>© 2024 CS-154 Smart Solar-Powered IoT Dehydrator | IIT | University of Westminster</p>
+</footer>
